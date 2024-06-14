45a9ac7485188d7d3d5c81fcf089f1317be08f8a
sudoku-DSA <vikashverma10102001@gmail.com>
2024-06-14 11:32:29 +0530

    encapsulation assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly rwonly =new RWOnly();
+//    System.out.println("Hello Bhai");
+    rwonly.setName("vikash");
+    System.out.println(rwonly.getName());
+  }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private static String name;
+
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        RWOnly.name = name;
+    }
 }
